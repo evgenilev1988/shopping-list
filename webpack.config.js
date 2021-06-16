af438efd42f743a99ccfb6120f07e6cc9341fe3e
Eugene Lev <eugenele@starstechnologies.com>
2021-06-15 21:09:14 -0400

    Updating webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: 'app/index.html'
         }),
-        new NodePolyfillPlugin()
+        new NodePolyfillPlugin(),
+        new CopyPlugin({ patterns: [{ from : 'app/images', to:'images' }] })
     ],
     devServer:{
         historyApiFallback:true
